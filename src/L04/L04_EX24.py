4c945cb0875ba6d82f37e9d5d2873a7960230a86
HeimdallCodeBr <heimdallcodebr@gmail.com>
2023-02-19 18:05:34 -0300

    --

--- src/L04/L04_EX24.py
+++ src/L04/L04_EX24.py
@@ -7,3 +7,20 @@
 # vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6)
 # e uma função para gerar numeros aleatórios, simulando os lançamentos dos
 # dados.
+
+from os import system
+from random import randint
+
+system('clear')
+
+dado = [1, 2, 3, 4, 5, 6]
+jogadas = [0] * 6
+
+for i in range(100):
+    a = randint(1, 6)
+    if a in dado:
+        jogadas[dado.index(a)] += 1
+print('Face Jogadas')
+for m in range(len(dado)):
+    print(f'{str(m+1).ljust(5)}{str(jogadas[m]).ljust(5)}')
+print(f'Total jogadas {sum(jogadas)}')
