6c3b4c539280936426f475d4c079e8f52fa48ea6
HeimdallCodeBr <heimdallcodebr@gmail.com>
2023-02-27 19:20:35 -0300

    --

--- src/L05/L13.py
+++ src/L05/L13.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from os import system
+from random import randint
 
 """Resolução Lista 05 Exercicio 13 Python Brasil (J.Siqueira 02/23)."""
 
@@ -17,6 +19,36 @@ para valores dentro da faixa de forma elegante.
 """
 
 
+def desenha_moldura(tc: int, tl: int) -> str:
+    if tc > 20 or tl > 20:
+        return str(None)
+    else:
+        desenho = ''
+        # SIMBOLOS
+        ca = '+'
+        sp = '  '
+        # COLUNAS
+        co = '--'
+        coluna = ca + (co * tc) + ca
+        # LINHAS
+        li = '|'
+        linha = ''
+        for i in range(tl):
+            linha += '\n' + li + (sp * tc) + li
+        desenho = coluna + linha + '\n' + coluna
+        return desenho
+
+
 # ==================
 # PROGRAMA PRINCIPAL
 # ==================
+while True:
+    system('clear')
+    for z in range(1):
+        tc = randint(1, 25)
+        tl = randint(1, 25)
+        print(f'Tamanho Coluna {tc} x Linha {tl}')
+        print(desenha_moldura(tc, tl))
+    s = input('Digite 0 para sair')
+    if s == '0':
+        break
