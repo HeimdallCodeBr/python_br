b509ab9a9c56a5271bb87a1d72ded04d886bcb91
HeimdallCodeBr <heimdallcodebr@gmail.com>
2023-06-23 09:03:54 -0300

    --

--- src/L08/L02.py
+++ src/L08/L02.py
@@ -5,9 +5,29 @@
 """
 2. Classe Quadrado:  Crie uma classe que modele um quadrado:
 
-a. Atributos: T amanho do lado
+a. Atributos: Tamanho do lado
 
 b. Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
 """
 
+
+class Quadrado():
+
+    def __init__(self, tamanho_lado):
+
+        self.tamanho_lado = tamanho_lado
+    
+    def muda_valor_lado(self):
+        pass
+
+    def retorna_valor_lado(self):
+        pass
+
+    def calcular_area(self):
+        pass
+
+
+q1 = Quadrado(10)
+
+q1.calcular_area()
