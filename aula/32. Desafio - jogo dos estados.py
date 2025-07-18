6d5d2c3586afd7545c7c20a73bdbc8d06d916a96
HeimdallCodeBr <heimdallcodebr@gmail.com>
2025-07-18 18:00:52 -0300

    --

--- aula/32. Desafio - jogo dos estados.py
+++ aula/32. Desafio - jogo dos estados.py
@@ -1,12 +1,16 @@
 import os
 os.system('clear')
 
-# Crie um "jogo dos estados". Neste jogo, o jogador precisa responder
-# o nome da capital de cada Estado do Brasil. O jogo deve perguntar
-# ao usuário "Qual a capital do Estado X?", e checar se o usuário
-# respondeu de forma correta. Após cada pergunta, o usuário pode escolher
-# parar o jogo ou continuar para a próxima pergunta. Quando o usuário
-# decidir parar, ou quando todas as perguntas forem respondidas,
+# Crie um "jogo dos estados". 
+# 
+# Neste jogo, o jogador precisa responder o nome da capital de cada Estado do Brasil.
+# 
+# O jogo deve perguntar ao usuário "Qual a capital do Estado X?", e checar se o usuário
+# respondeu de forma correta. 
+# 
+# Após cada pergunta, o usuário pode escolher parar o jogo ou continuar para a próxima pergunta.
+# 
+# Quando o usuário decidir parar, ou quando todas as perguntas forem respondidas,
 # o código mostra o número bruto e porcentagem de acertos.
 
 
@@ -40,7 +44,51 @@ estados = {
 }
 
 
-print(estados["Tocantins"])
 
+n = ''
+continua = ''
+digitados = []
+tentativa = 0
+tentativas = 3
+t = len(estados)
+erros = 0
+acertos = 0
+total = 0
+percetual = 0
 
-print('\n')
+while True:
+    digitados.append(n)
+    n = int(input(f'\nDigite um numero entre 0 e {t}: '))
+    if (n in digitados):
+        print(f'numero {n} já foi digitado!, digite novamente: ')
+    elif (n < 0 or n >= t):
+        print(f'Valor digitado incorreto! você tem {tentativas-tentativa-1} tentativa(s)')
+        tentativa += 1
+        if tentativa >= tentativas:
+            print('\n**********************')
+            print('* * * GAME OVER! * * *')
+            print('**********************\n')
+            break
+        
+    
+    for i, estado in enumerate(estados):
+        if i == n:
+            capital = input(f'Qual é a capital de {estado}? ')
+            if capital == estados.get(estado):
+                print('Acertou\n')
+                acertos +=1
+            else:
+                print('Errou!\n')
+                erros +=1
+    
+    continua = input('Deseja continuar? S/N: ')
+    if continua in 'Nn' or continua =='':
+        total = acertos+erros
+        percentual = (acertos/total)*100
+        print ('\n---------------------------------')
+        print (f'Você respondeu total de {total} perguntas')
+        print (f'{acertos} ACERTOS')
+        print (f'{erros} ERROS')
+        print (f'Aproveitamento de {percentual:.0f}%')
+        print ('---------------------------------\n')
+        break
